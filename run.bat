28c3b739bc499f9350b37312585cc4d3d8dec0c8
Viglofix <viglofix@gmail.com>
2023-11-20 14:27:56 +0100

    config3commit

--- run.bat
+++ run.bat
@@ -3,17 +3,15 @@ setlocal
 rem Pobieranie aktualnej sciezki 
 set "sciezka=%~dp0"
 
-set "podfolder=FormApiSolution\DataBase"
 set "podfolder2=FormApiSolution\FormAPI"
 
-rem set "sciezka=%sciezka%%podfolder%"
 set "sciezka=%sciezka%%podfolder2%"
 
 rem przjesce do nowej sciezki
 
 cd /d "%sciezka%"
 rem dotnet ef migrations add -s ..\FormAPI\FormAPI.csproj "SchoolMigration"
-dotnet ef database update -s ..\FormAPI\FormAPI.csproj
+rem dotnet ef database update -s ..\FormAPI\FormAPI.csproj
 
 dotnet run
 rem http://localhost:5276/swagger/index.html
